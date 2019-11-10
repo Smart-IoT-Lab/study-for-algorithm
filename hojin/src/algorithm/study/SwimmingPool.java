33a4140cffd5c51e2a33607dc694bec924fee18d
cow_notebook <h_hojin@naver.com>
2019-11-10 11:53:40 +0900

    change swimming problem

--- hojin/src/algorithm/study/SwimmingPool.java
+++ hojin/src/algorithm/study/SwimmingPool.java
@@ -6,7 +6,10 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
+import java.util.ArrayList;
+
 public class SwimmingPool {
+	static int vertex, edge, S;
 	public void solution()
 	{
 		
@@ -21,22 +24,35 @@ public class SwimmingPool {
 			
 			BufferedReader bufReader = new BufferedReader(fileReader);
 			
-			String line = "";
-			
 			String testCase = bufReader.readLine();
 			int numOfTestCase = Integer.parseInt(testCase);
 			for(int roop = 0; roop < numOfTestCase; roop++)
 			{
 				int poolSize = Integer.parseInt(bufReader.readLine());
+				System.out.println(poolSize);
+				
+				ArrayList<String> adj = new ArrayList<>(); 
+				
+				for(int i = 0; i < poolSize; i++)
+				{
+					adj.add(bufReader.readLine());
+					
+				}
+				String startPosition = bufReader.readLine();
+				String escapePosiont = bufReader.readLine();
+				System.out.println(startPosition);
+				System.out.println(escapePosiont);
+				
 				int moveCount = 0;
+				System.out.println(adj);
 				while(true)
 				{
 					// TODO exit
 					moveCount += 1;
-					if(moveCount == 100)
+					if(moveCount == 2)
 						break;
 				}
-				System.out.println("#"+roop+1+" "+moveCount);
+				System.out.println("#"+(roop+1)+" "+moveCount);
 			}
 			bufReader.close();
 		}
