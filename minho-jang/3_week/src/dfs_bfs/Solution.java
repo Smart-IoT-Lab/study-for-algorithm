7118fea84d7e53fc32f3a1f301d98916e8cf9188
minho <minho9301@naver.com>
2019-11-23 20:18:00 +0900

    Solve Problems and Summary of 3_week

--- minho-jang/3_week/src/dfs_bfs/Solution.java
+++ minho-jang/3_week/src/dfs_bfs/Solution.java
@@ -1,10 +1,40 @@
 package dfs_bfs;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Solution {
 	public int solution(int n, int[][] computers) {
         int answer = 0;
         
+        // BFS
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] visited = new boolean[n];
+        Arrays.fill(visited, false);
         
+        for (int start = 0; start < n; start++) {
+        	// 방문한적 있으면 건너뛰어
+        	if(visited[start])
+        		continue;
+        	
+            q.add(start);
+            visited[start] = true;
+            
+            while(!q.isEmpty()) {
+            	
+            	start = q.poll();
+            	for (int i = 0; i < n; i++) {
+            		if (start != i && computers[start][i] == 1 && !visited[i]) {
+            			q.add(i);
+            			visited[i] = true;
+            		}
+            	}
+            }
+            
+            // 여기까지 왔다면 하나의 네트워크가 있다는 증거
+            answer++;
+        }
         
         return answer;
     }
@@ -13,8 +43,8 @@ public class Solution {
 		int n = 3;
 		int[][] computers = {
 				{1,1,0},
-				{1,1,0},
-				{0,0,1}
+				{1,1,1},
+				{0,1,1}
 		};
 		
 		Solution s = new Solution();
