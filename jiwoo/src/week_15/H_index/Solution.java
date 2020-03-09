cf0d421e38120e217bc781ca2f0835ed838362a7
eraserrr <a01074761054@gmail.com>
2020-03-09 20:10:17 +0900

    h index

--- jiwoo/src/week_15/H_index/Solution.java
+++ jiwoo/src/week_15/H_index/Solution.java
@@ -13,15 +13,29 @@ public class Solution {
         Arrays.parallelSort(citations);
         for (int i = citations.length - 1; i >= 0; i--) {
             ref = citations[i];
-            if (idx >= ref || idx <= ref)
+            //System.out.println(idx+"번째 탐색");
+            // ref 기준
+            if (idx >= ref) {
+                //System.out.println("ref 에 따라서 정해짐");
                 return ref;
-            idx = idx + 1;
+            }
+            // idx 기준
+            else if(i!=0 && idx > citations[i-1]){
+                //System.out.println("idx:"+idx+"에 따라서 정해짐");
+                return idx;
+            }
+            else if(i==0) {
+                // idx가 마지막까지갈때동안 ref보다 크지 못했을때
+                // 즉 배열의 최소값이 배열의 사이즈보다 작을 때
+                return citations.length;
+            }
+            idx += 1;
         }
         return 0;
     }
 
     public static void main(String[] args) {
-        int[] numbers = { 5, 0 };
+        int[] numbers = { 42,22};
         System.out.println(new Solution().solution(numbers));
     }
 }
