64d68a68a10992c08ec925b8b360b912fd41be03
선민기 <suns1502@gmail.com>
2020-11-30 10:54:53 +0900

    4주차

--- minky/4주차/Solution.java
+++ minky/4주차/Solution.java
@@ -1,21 +1,38 @@
-package dp;
+package week4;
 
-class Solution {
-    public int solution(int[] money) {
-        int maxanswer = 0;
-        int[] answer = {money[0],money[1]};
+import java.util.Arrays;
 
-        for(int i = 2;i < money.length;i++){
-            answer[i%2] += money[i];
-            maxanswer = answer[i%2]= getMax(answer[i%2],answer[(i-1)%2]);
-        }
-
-        return maxanswer;
+class Thief {
+    public static void main(String[] args) {
+        System.out.println(new Thief().solution(new int[]{1, 5, 10, 1, 7, 9}));
     }
 
-    private int getMax(int x,int y){
-        if(x > y)
-            return x;
-        return y;
+    public int solution(int[] money) {
+        int[] accumulated_value = new int[money.length];
+        int[] answer = {0, 0};
+
+        /*1 ~ money.length-1 개까지 중 최대값 구하기*/
+        accumulated_value[0] = 0;   //맨 앞에 있는 집은 안털거임
+        accumulated_value[1] = money[1];
+        accumulated_value[2] = money[2];
+
+        for(int i=3;i < money.length;i++){
+            accumulated_value[i] = Integer.max(accumulated_value[i-3], accumulated_value[i-2]) + money[i];
+        }
+
+        answer[0] = Integer.max(accumulated_value[money.length-1], accumulated_value[money.length-2]);  //1 ~ money.length-1 개까지 중 최대값
+        
+        /*0 ~ money.length-2 개까지 중 최대값 구하기*/
+        accumulated_value[0] = money[0];    //맨 앞에있는 집을 털거임
+        accumulated_value[2] = money[0] + money[2]; //맨 앞에있는 집을 털어서 2번째 집에서 얻을 수 있는 최대값도 바뀜
+
+        for(int i=3;i < money.length;i++){
+            accumulated_value[i] = Integer.max(accumulated_value[i-3], accumulated_value[i-2]) + money[i];
+        }
+
+        answer[1] = Integer.max(accumulated_value[money.length-2], accumulated_value[money.length-3]);  //0 ~ money.length-2 개까지 중 최대값
+
+        Arrays.sort(answer);
+        return answer[1];   //제일 큰 애가 제일 뒤에 있음
     }
 }
